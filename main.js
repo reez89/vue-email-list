6d091ab826dc0336d8dda6c9dba8c1aa22b296d5
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-08 15:46:11 +0100

    Finish
    Corretto impaginazione.

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let app = new Vue({
     el: "#app",
     data : {
-        mailGenerator: [
+        mailGenerator: 
+        [
 
         ]
     },
 
 
     mounted() {
+
         for (let i=0 ;i<10;i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response =>{
                 let mailsApi = response.data.response   
                 let newMail = {
                 mail : mailsApi
-            }
-            this.mailGenerator.push(newMail);
-                
+                }
+                this.mailGenerator.push(newMail);    
             });
         }
     },
