84aef754b969a5e109197cd021fd4d3cbe2e1488
Nicola R <nicola.rizzi89@hotmail.it>
2021-01-08 15:38:29 +0100

    Finish
    Exercise completed.

---
+++ main.js
@@ -0,0 +1,23 @@
+let app = new Vue({
+    el: "#app",
+    data : {
+        mailGenerator: [
+
+        ]
+    },
+
+
+    mounted() {
+        for (let i=0 ;i<10;i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response =>{
+                let mailsApi = response.data.response   
+                let newMail = {
+                mail : mailsApi
+            }
+            this.mailGenerator.push(newMail);
+                
+            });
+        }
+    },
+
+});
